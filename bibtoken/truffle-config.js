2cccc9483fc16a10b917dea0e50a8860bd2bb008
Bibhash Singh <bibhash1996@gmail.com>
2021-12-06 20:56:33 +0530

    deploy script

--- bibtoken/truffle-config.js
+++ bibtoken/truffle-config.js
@@ -1,3 +1,6 @@
+const { projectId, mnemonic } = require('./secrets.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
   networks: {
     development: {
@@ -6,6 +9,14 @@ module.exports = {
       network_id: "*", // Match any network id
       websockets:true
     },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    }
   },
   contracts_directory: './contracts/',
   contracts_build_directory: './src/abis/',
